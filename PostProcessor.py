abdb24dc0982f9699dab785c0ebc35d5fe8912aa
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-21 19:26:24 +0900

    [add] Implemented the function which writes the submission file

--- PostProcessor.py
+++ PostProcessor.py
@@ -1,5 +1,12 @@
 #coding:utf-8
 
+import numpy as np
+
 class PostProcessor:
     def write_submission_file(self, prediction_collection, filename):
-        raise NotImplementedError()
+        ids = prediction_collection.get_ids()
+        predicted_labels = prediction_collection.get_predicted_labels()
+        
+        submission_data = np.array([ids, predicted_labels]).transpose()
+        header = "Id,Response"
+        np.savetxt(filename, submission_data, fmt="%.0f", delimiter=",", header=header, comments="")
