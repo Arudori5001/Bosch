9c38a48edad986805d8248dfa67b7b8e9da1be7a
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-26 02:03:59 +0900

    [modify] Reflected how to use Dataset class

--- SvmModel.py
+++ SvmModel.py
@@ -18,17 +18,24 @@ class SvmModel(Model):
     
     
     def learn(self, train_dataset, valid_dataset):
-        input_matrix = train_dataset.get_input_matrix()
-        labels = train_dataset.get_labels()
-        
-        self.__get_classifier().fit(input_matrix, labels)
+        for chunk in train_dataset.get_record_chunks_generator():
+            input_matrix = train_dataset.get_input_matrix(chunk)
+            labels = train_dataset.get_labels(chunk)
+            self.__get_classifier().fit(input_matrix, labels)
         
         
     def predict(self, dataset):
-        records = dataset.get_records()
-        input_matrix = dataset.get_input_matrix()
-        predicted_labels = self.__get_classifier().predict(input_matrix)
-        predictions = np.array([Prediction(record=r,predicted_label=l) for r,l in zip(records, predicted_labels)])
+        predictions = []
+        for chunk in dataset.get_record_chunks_generator():
+            ids = dataset.get_ids(chunk)
+            input_matrix = dataset.get_input_matrix(chunk)
+            actual_labels = dataset.get_labels(chunk)
+            predicted_labels = self.__get_classifier().predict(input_matrix)
+            
+            for i,(id,a,p) in enumerate(zip(ids,actual_labels,predicted_labels)):
+                prediction = Prediction(id=id,actual_label=a,predicted_label=p)
+                predictions.append(prediction)
+            
         prediction_collection = PredictionCollection(predictions)
         return prediction_collection
     
