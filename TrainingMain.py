66d3f947525373df0b35540d7394224259627d52
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-26 02:01:49 +0900

    [modify] Reflected how to use Dataset class

--- TrainingMain.py
+++ TrainingMain.py
@@ -6,15 +6,23 @@ from Dataset import Dataset
 from SvmModel import SvmModel
 
 @click.command()
-@click.argument("train_dataset_file")
-@click.argument("valid_dataset_file")
 @click.argument("model_file")
-def main(train_dataset_file, valid_dataset_file, model_file):
-    train_dataset = Dataset.load(train_dataset_file)
-    valid_dataset = Dataset.load(valid_dataset_file)
+def main(model_file):
+    host = "localhost"
+    db_name = "bosch"
+    username = "root"
+    passward = ""
+    train_table_name = "train_processed"
+    valid_table_name = "valid_processed"
+    chunksize = 10
+    
+    train_dataset = Dataset(is_test=False,host=host,db_name=db_name,username=username,passward=passward,table_name=train_table_name,chunksize=chunksize)
+    valid_dataset = Dataset(is_test=False,host=host,db_name=db_name,username=username,passward=passward,table_name=valid_table_name,chunksize=chunksize)
     model = SvmModel()
     model.learn(train_dataset, valid_dataset)
     model.save(model_file)
+    train_dataset.dispose()
+    valid_dataset.dispose()
     
 if __name__ == "__main__":
     main()
