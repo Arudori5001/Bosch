857f67fc6486dbcc9bc56554abe9c64884216710
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-25 16:49:50 +0900

    [remove] Removed the functions of Dataset for introduction of MySQL

--- Dataset.py
+++ Dataset.py
@@ -1,35 +1,17 @@
 #coding:utf-8
 
-import cPickle
 import numpy as np
 
-from ISavable import ISavable
+from IDisposable import IDisposable
 
-class Dataset(ISavable):
-    def __init__(self, records):
-        self.__records = records
-        
-    
-    def get_records(self):
-        return self.__records
-    
-    
-    @staticmethod
-    def load(filepath):
-        obj = None
-        with open(filepath, "rb") as f:
-            obj = cPickle.load(f)
-        return obj
-    
-    
-    def save(self, filepath):
-        with open(filepath, "wb") as f:
-            cPickle.dump(self, f)
+class Dataset(IDisposable):
+    def __init__(self, host, db, user, passward):
+        raise NotImplementedError()
 
 
     def get_input_matrix(self):
-        return np.array([r.get_input_vector() for r in self.get_records()])
+        raise NotImplementedError()
 
     
     def get_labels(self):
-        return np.array([r.get_label() for r in self.get_records()])
+        raise NotImplementedError()
