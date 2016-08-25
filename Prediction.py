c2e293efc83ff2802d3374eb0e787ceb4afede80
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-26 01:56:16 +0900

    [modify] Let Prediction class to have id and actual label instead of record instance

--- Prediction.py
+++ Prediction.py
@@ -1,12 +1,19 @@
 #coding:utf-8
 
 class Prediction:
-    def __init__(self, record, predicted_label):
-        self.__record = record
+    def __init__(self, id, actual_label, predicted_label):
+        self.__id = id
+        self.__actual_label = actual_label
         self.__predicted_label = predicted_label
     
-    def get_record(self):
-        return self.__record
+    
+    def get_id(self):
+        return self.__id
+    
+    
+    def get_actual_label(self):
+        return self.__actual_label
+        
     
     def get_predicted_label(self):
         return self.__predicted_label
