068463b48a665fbb31ab139a33ed7c95ee674d1a
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-26 20:23:49 +0900

    [modify] Reflected Preprocessor class to how to use DatabaseManager class

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,50 +1,23 @@
 #coding:utf-8
 
-import numpy as np
-
-from Dataset import Dataset
-from Record import Record
+import datetime
 
 class Preprocessor:
-    def __init__(self, configuration):
-        self.__configuration = configuration
+    def __init__(self, databasemanager):
+        self.__database_manager = databasemanager
         
         
-    def __get_configuration(self):
-        return self.__configuration
+    def __get_database_maneger(self):
+        return self.__database_manager
     
     
-    def preprocess(self, train_raw_dataset, test_raw_dataset):
-        train_raw_dataset.fill_missing(0)
-        test_raw_dataset.fill_missing(0)
-        
-    
-    def get_train_dataset(self, train_raw_dataset):
-        config = self.__get_configuration()
-        
-        train_raw_records = train_raw_dataset.get_records()
-        train_processed_records = train_raw_records[:config.get_train_num()]
-        train_processed_records = np.array([Record.create_from_raw_record(raw_record) for raw_record in train_processed_records])
-        train_processed_dataset = Dataset(train_processed_records)
-        return train_processed_dataset
-    
-    
-    def get_valid_dataset(self, train_raw_dataset):
-        config = self.__get_configuration()
-        
-        train_raw_records = train_raw_dataset.get_records()
-        valid_processed_records = train_raw_records[config.get_train_num() : config.get_train_num() + config.get_valid_num()]
-        valid_processed_records = np.array([Record.create_from_raw_record(raw_record) for raw_record in valid_processed_records])
-        valid_processed_dataset = Dataset(valid_processed_records)
-        return valid_processed_dataset
-    
-    
-    def get_test_dataset(self, test_raw_dataset):
-        config = self.__get_configuration()
-        
-        test_raw_records = test_raw_dataset.get_records()
-        test_processed_records = test_raw_records[:config.get_test_num()]
-        test_processed_records = np.array([Record.create_from_raw_record(raw_record) for raw_record in test_processed_records])
-        test_processed_dataset = Dataset(test_processed_records)
-        return test_processed_dataset
-        
+    def preprocess(self):
+        manager = self.__get_database_maneger()
+        config = manager.get_configuration()
+
+        manager.truncate(config.get_processed_train_table_name())
+        manager.truncate(config.get_processed_valid_table_name())
+        manager.truncate(config.get_processed_test_table_name())
+
+        manager.pick_up_randomly_from_raw_train()
+        manager.copy(config.get_raw_test_table_name(), config.get_processed_test_table_name())
