3228ff7ffef0440b224c35dbd4066be225ae183a
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-08-26 20:38:31 +0900

    [modify] Renamed PostProcessor to Postprocessor

--- PostprocessMain.py
+++ PostprocessMain.py
@@ -3,14 +3,14 @@
 import click
 
 from PredictionCollection import PredictionCollection
-from PostProcessor import PostProcessor
+from Postprocessor import Postprocessor
 
 @click.command()
 @click.argument("prediction_file")
 @click.argument("submission_file")
 def main(prediction_file, submission_file):
     prediction_collection = PredictionCollection.load(prediction_file)
-    postprocessor = PostProcessor()
+    postprocessor = Postprocessor()
     postprocessor.write_submission_file(prediction_collection, submission_file)
 
 if __name__ == '__main__':
--- Postprocessor.py
+++ Postprocessor.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-class PostProcessor:
+class Postprocessor:
     def write_submission_file(self, prediction_collection, filename):
         ids = prediction_collection.get_ids()
         predicted_labels = prediction_collection.get_predicted_labels()
