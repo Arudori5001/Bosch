9e51078823de8cbd8805959459b8ead75f73e8dd
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-02 19:24:53 +0900

    [modify] Reflected the change about using id

--- PredictionMain.py
+++ PredictionMain.py
@@ -3,15 +3,18 @@
 import click
 import configparser
 
-from Dataset import Dataset
 from Model import Model
+from LabeledDataset import LabeledDataset
+from UnlabeledDataset import UnlabeledDataset
 
 @click.command()
 @click.argument("table_name")
+@click.argument("id_table_name")
 @click.argument("is_test",type=bool)
 @click.argument("model_file")
 @click.argument("prediction_file")
-def main(table_name,is_test,model_file,prediction_file):
+def main(table_name,id_table_name,is_test,model_file,prediction_file):
+
     setting_parser = configparser.ConfigParser()
     setting_parser.read("setting.ini")
     host = setting_parser.get("database", "host_name")
@@ -20,12 +23,18 @@ def main(table_name,is_test,model_file,prediction_file):
     passward = setting_parser.get("database", "password")
 
     chunksize = 10 ** 4
-    
-    dataset = Dataset(is_test=is_test,host=host,db_name=db_name,username=username,passward=passward,table_name=table_name,chunksize=chunksize)
+
+    dataset = None
+    if is_test:
+        dataset = UnlabeledDataset(host=host,db_name=db_name,username=username,passward=passward,table_name=table_name,chunksize=chunksize)
+    else:
+        dataset = LabeledDataset(host=host,db_name=db_name,username=username,passward=passward,table_name=table_name,id_table_name=id_table_name,chunksize=chunksize)
+
     model = Model.load(model_file)
     
     prediction_collection = model.predict(dataset)
     prediction_collection.save(prediction_file)
+
     
 
 if __name__ == "__main__":
