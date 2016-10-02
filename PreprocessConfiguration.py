53a664e0b0ac8bd883598e405995c1c8a45a3401
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-02 18:53:39 +0900

    [modify] Let PreprocessConfiguration to have id table used by database

--- PreprocessConfiguration.py
+++ PreprocessConfiguration.py
@@ -1,30 +1,19 @@
 #coding:utf-8
 
+import configparser
+
+
 class PreprocessConfiguration:
     def __init__(self,
-                 train_num, valid_num, test_num,
                  raw_train_table_name,raw_test_table_name,
-                 processed_train_table_name,processed_valid_table_name,processed_test_table_name):
-        self.__train_num = train_num
-        self.__valid_num = valid_num
-        self.__test_num = test_num
+                 processed_train_table_name,processed_test_table_name,
+                 processed_train_id_table_name, processed_valid_id_table_name):
         self.__raw_train_table_name = raw_train_table_name
         self.__raw_test_table_name = raw_test_table_name
         self.__processed_train_table_name = processed_train_table_name
-        self.__processed_valid_table_name = processed_valid_table_name
         self.__processed_test_table_name = processed_test_table_name
-        
-        
-    def get_train_num(self):
-        return self.__train_num
-    
-    
-    def get_valid_num(self):
-        return self.__valid_num
-    
-    
-    def get_test_num(self):
-        return self.__test_num
+        self.__processed_train_id_table_name = processed_train_id_table_name
+        self.__processed_valid_id_table_name = processed_valid_id_table_name
 
 
     def get_raw_train_table_name(self):
@@ -39,9 +28,14 @@ class PreprocessConfiguration:
         return self.__processed_train_table_name
 
 
-    def get_processed_valid_table_name(self):
-        return self.__processed_valid_table_name
-
-
     def get_processed_test_table_name(self):
         return self.__processed_test_table_name
+
+
+    def get_processed_train_id_table_name(self):
+        return self.__processed_train_id_table_name
+
+
+    def get_processed_valid_id_table_name(self):
+        return self.__processed_valid_id_table_name
+
