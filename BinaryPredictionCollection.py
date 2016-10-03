e64c0d2a201b485d2bd3d52ffd4f9e979501aa80
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-03 14:44:59 +0900

    [modify] Divided the functions of PredictionCollection and moved the functions for only binary-classification into new class BinaryPredictionCollection

---
+++ BinaryPredictionCollection.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+
+import numpy as np
+
+from PredictionCollection import PredictionCollection
+
+class BinaryPredictionCollection(PredictionCollection):
+
+    def __init__(self,predictions):
+        super(BinaryPredictionCollection,self).__init__(predictions)
+
+    def get_accuracy(self):
+        tp = self.get_confusion_matrix()[1,1]
+        fp = self.get_confusion_matrix()[0,1]
+        tn = self.get_confusion_matrix()[0,0]
+        fn = self.get_confusion_matrix()[1,0]
+
+        return float(tp + tn) / (tp + fp + tn + fn)
+
+    def get_mcc(self):
+        """
+        @summary: calculate Matthew Correlation Coefficient(MCC)
+        """
+        tp = self.get_confusion_matrix()[1,1]
+        fp = self.get_confusion_matrix()[0,1]
+        tn = self.get_confusion_matrix()[0,0]
+        fn = self.get_confusion_matrix()[1,0]
+
+        return float(tp * tn - fp * fn) / np.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
--- PredictionCollection.py
+++ PredictionCollection.py
@@ -1,85 +1,48 @@
-#coding:utf-8
+# coding:utf-8
 
 import cPickle
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 from ISavable import ISavable
 
+
 class PredictionCollection(ISavable):
+
     def __init__(self, predictions):
         self.__predictions = predictions
-    
-    
+
     def get_predictions(self):
         return self.__predictions
-    
-    
+
     @staticmethod
     def load(filepath):
         obj = None
         with open(filepath, "rb") as f:
             obj = cPickle.load(f)
         return obj
-    
-    
+
     def save(self, filepath):
         with open(filepath, "wb") as f:
             cPickle.dump(self, f)
-            
-            
+
     def get_ids(self):
         return np.array([p.get_id() for p in self.get_predictions()])
-
 
     def get_actual_labels(self):
         return np.array([p.get_actual_label() for p in self.get_predictions()])
 
-
     def get_predicted_labels(self):
         return np.array([p.get_predicted_label() for p in self.get_predictions()])
-    
-    
-    def __get_tp_num(self):
-        actual_trues = (self.get_actual_labels() == 1)
-        predicted_trues = (self.get_predicted_labels() == 1)
-        return np.count_nonzero(np.logical_and(actual_trues, predicted_trues))
-    
-    
-    def __get_fp_num(self):
-        actual_falses = (self.get_actual_labels() == 0)
-        predicted_trues = (self.get_predicted_labels() == 1)
-        return np.count_nonzero(np.logical_and(actual_falses, predicted_trues))
-    
-    
-    def __get_tn_num(self):
-        actual_falses = (self.get_actual_labels() == 0)
-        predicted_falses = (self.get_predicted_labels() == 0)
-        return np.count_nonzero(np.logical_and(actual_falses, predicted_falses))
-    
-    
-    def __get_fn_num(self):
-        actual_trues = (self.get_actual_labels() == 1)
-        predicted_falses = (self.get_predicted_labels() == 0)
-        return np.count_nonzero(np.logical_and(actual_trues, predicted_falses))
-    
-    
-    def get_accuracy(self):
-        tp = self.__get_tp_num()
-        fp = self.__get_fp_num()
-        tn = self.__get_tn_num()
-        fn = self.__get_fn_num()
-        
-        return float(tp + tn) / (tp + fp + tn + fn)
-    
-    
-    """
-    @summary: calcrate Matthew Correlation Coefficient(MCC)
-    """
-    def get_mcc(self):
-        tp = self.__get_tp_num()
-        fp = self.__get_fp_num()
-        tn = self.__get_tn_num()
-        fn = self.__get_fn_num()
-        
-        return float(tp * tn - fp * fn) / np.sqrt((tp + fp)*(tp + fn)*(tn + fp)*(tn + fn))
-        
+
+    def get_confusion_matrix(self):
+        actuals = self.get_actual_labels()
+        predicteds = self.get_predicted_labels()
+        label_kinds = self.get_label_kinds()
+
+        return confusion_matrix(actuals, predicteds, labels=label_kinds)
+
+    def get_label_kinds(self):
+        seen = set()
+        label_kinds = sorted([x for x in self.get_actual_labels() if x not in seen and not seen.add(x)])
+        return label_kinds
