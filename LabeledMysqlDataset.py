fdca5a1f0896fc8eca40249666a16c281a3172fb
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-03 14:39:07 +0900

    [modify] Modified the access level of mysql dataset classes

--- LabeledMysqlDataset.py
+++ LabeledMysqlDataset.py
@@ -8,19 +8,18 @@ from Record import Record
 class LabeledMysqlDataset(MysqlDataset):
 
     def __init__(self, host, db_name, username, passward, table_name, id_table_name, chunksize):
-        super.__init__(host, db_name, username, passward, table_name, chunksize)
+        super(LabeledMysqlDataset,self).__init__(host, db_name, username, passward, table_name, chunksize)
         self.__id_table_name = id_table_name
 
 
     def __get_id_table_name(self):
         return self.__id_table_name
 
-
-    def __get_table(self, offset, num):
-        cursor =  self.__get_db_host().cursor()
-        query = """select t.* from (select Id from {0} limit {1} offset {2}) as i, {3} as t
-            where i.Id = t.Id;
-        """.format(self.__get_id_table_name(), num, offset, self.__get_table_name())
+    def get_table(self, offset, num):
+        cursor =  self.get_db_host().cursor()
+        query = """select t.* from (select id from {0} limit {1} offset {2}) as i, {3} as t
+            where i.id = t.id;
+        """.format(self.__get_id_table_name(), num, offset, self.get_table_name())
         cursor.execute(query)
         result = cursor.fetchall()
         cursor.close()
@@ -28,15 +27,13 @@ class LabeledMysqlDataset(MysqlDataset):
         result = np.array(result)
         return result
 
-
-    def __get_records_with_range(self, offset, num):
-        table = self.__get_table(offset, num)
+    def get_records_with_range(self, offset, num):
+        table = self.get_table(offset, num)
         records = np.array([Record(id=row[0],input_vector=row[1:-1],label=row[-1]) for row in table])
         return records
 
-
-    def __get_records_num(self):
-        cursor =  self.__get_db_host().cursor()
+    def get_records_num(self):
+        cursor =  self.get_db_host().cursor()
         query = "select count(*) from {0}".format(self.__get_id_table_name())
         cursor.execute(query)
         result = cursor.fetchall()
--- MysqlDataset.py
+++ MysqlDataset.py
@@ -4,10 +4,11 @@ import abc
 import numpy as np
 import MySQLdb
 
+from AbstractDataset import AbstarctDataset
 from IDisposable import IDisposable
 
 
-class MysqlDataset(IDisposable):
+class MysqlDataset(AbstarctDataset,IDisposable):
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, host, db_name, username, passward, table_name, chunksize):
@@ -15,37 +16,37 @@ class MysqlDataset(IDisposable):
         self.__dbhost = MySQLdb.connect(host=host, db=db_name, user=username, passwd=passward, charset="utf8")
         self.__chunksize = chunksize
 
-    def __get_db_host(self):
+    def get_db_host(self):
         return self.__dbhost
 
-    def __get_table_name(self):
+    def get_table_name(self):
         return self.__table_name
 
-    def __get_chunksize(self):
+    def get_chunksize(self):
         return self.__chunksize
 
     @abc.abstractmethod
-    def __get_table(self, offset, num):
+    def get_table(self, offset, num):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def __get_records_with_range(self, offset, num):
+    def get_records_with_range(self, offset, num):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def __get_records_num(self):
+    def get_records_num(self):
         raise NotImplementedError()
 
     def get_records(self):
-        num = self.__get_records_num()
-        return self.__get_records_with_range(0,num)
+        num = self.get_records_num()
+        return self.get_records_with_range(0,num)
 
     def get_record_chunks_generator(self):
-        chunksize = self.__get_chunksize()
-        records_num = self.__get_records_num()
+        chunksize = self.get_chunksize()
+        records_num = self.get_records_num()
 
         for offset in range(0, records_num, chunksize):
-            yield self.__get_records(offset, chunksize)
+            yield self.get_records_with_range(offset, chunksize)
 
     def get_ids_from_chunk(self, chunk):
         return np.array([r.get_id() for r in chunk])
@@ -57,4 +58,4 @@ class MysqlDataset(IDisposable):
         return np.array([r.get_label() for r in chunk])
 
     def dispose(self):
-        self.__get_db_host().close()
+        self.get_db_host().close()
