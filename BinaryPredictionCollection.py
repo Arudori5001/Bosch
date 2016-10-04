750e030acdd16161463d0a878fca4711ba1487e5
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-04 11:03:59 +0900

    [modify] Let BinaryPredictionCollection get confusion matrix at first and next calculate true positive and so on

--- BinaryPredictionCollection.py
+++ BinaryPredictionCollection.py
@@ -10,10 +10,11 @@ class BinaryPredictionCollection(ClassificationPredictionCollection):
         super(BinaryPredictionCollection,self).__init__(predictions)
 
     def get_accuracy(self):
-        tp = self.get_confusion_matrix()[1,1]
-        fp = self.get_confusion_matrix()[0,1]
-        tn = self.get_confusion_matrix()[0,0]
-        fn = self.get_confusion_matrix()[1,0]
+        confusion_matrix = self.get_confusion_matrix()
+        tp = confusion_matrix[1,1]
+        fp = confusion_matrix[0,1]
+        tn = confusion_matrix[0,0]
+        fn = confusion_matrix[1,0]
 
         return float(tp + tn) / (tp + fp + tn + fn)
 
@@ -21,9 +22,10 @@ class BinaryPredictionCollection(ClassificationPredictionCollection):
         """
         @summary: calculate Matthew Correlation Coefficient(MCC)
         """
-        tp = self.get_confusion_matrix()[1,1]
-        fp = self.get_confusion_matrix()[0,1]
-        tn = self.get_confusion_matrix()[0,0]
-        fn = self.get_confusion_matrix()[1,0]
+        confusion_matrix = self.get_confusion_matrix()
+        tp = confusion_matrix[1,1]
+        fp = confusion_matrix[0,1]
+        tn = confusion_matrix[0,0]
+        fn = confusion_matrix[1,0]
 
         return float(tp * tn - fp * fn) / np.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
